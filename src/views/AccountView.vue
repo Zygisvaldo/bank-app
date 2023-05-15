900d2b65fff96c67b51e7757b1cbfbd8ba01cf89
Zygisvaldo <zygiodeze@gmail.com>
2023-05-15 18:12:41 +0300

    init

---
+++ src/views/AccountView.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn color="primary">Click me!</v-btn>
+</template>
+
+<script>
+
+
+export default {
+  
+}
+</script>
---
+++ src/views/SignupView.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn color="secondary">Click me!</v-btn>
+</template>
+
+<script>
+
+
+export default {
+  
+}
+</script>
